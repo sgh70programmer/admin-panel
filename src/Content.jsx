e73d6fc37112763414f90db33adcfa0569cd6e42
sadegh <sadegh_ghorbani@yahoo.com>
2024-07-07 00:16:57 +0330

    post-reducer

--- src/Content.jsx
+++ src/Content.jsx
@@ -7,6 +7,7 @@ import AddUser from './users/AddUser';
 import EditDesc from './users/EditDesc';
 import Gallery from './gallery/Gallery';
 import Posts from './posts/Posts';
+import AddPost from './posts/AddPost';
 import style from './style.module.css'
 
 
@@ -29,13 +30,15 @@ const Content = () => {
                 onClick={handleShowMenu}
             ></i>
             <Routes>
-                {/* <Route path="/" element={<Navigate replace to="/gallery"/>} /> */}
+                
                 <Route path="/user" element={<Users />} />
                 <Route path="/user/add" element={<AddUser />}>
                     <Route path=":userId" />
-                    {/* <Route path=":userId" element={<EditDesc/>} /> */}
                 </Route>
                 <Route path="/post" element={<Posts />} />
+                <Route path="/post/add" element={<AddPost/>}>
+                        <Route path=":postId"/>
+                    </Route>
                 <Route path="/gallery" element={<Gallery />} />
                 <Route path="/todo" element={<Todos />} />
                 <Route path="*" element={<Users />} />
